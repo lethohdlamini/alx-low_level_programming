a2330cc2df9b62f6678c8bc2083ef38854d2b84c
letho <lethohdlamini@gmail.com>
2023-07-19 11:38:36 -0700

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,7 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -23,3 +24,4 @@ int print_sign(int n)
 	}
 		return (-1);
 }
+
